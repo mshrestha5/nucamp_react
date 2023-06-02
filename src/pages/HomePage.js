ee84fdb46d25786d9f68dc655bf9387408f1f60f
Matina Shrestha <shresthamatina21@gmail.com>
2023-06-02 14:12:21 -0500

    Homepage Display

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,12 @@
-import React from "react";
+import { Container } from "reactstrap";
+import DisplayList from "../features/display/DisplayList";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <Container>
+      <DisplayList />
+    </Container>
+  );
 };
 
 export default HomePage;
