c17aadf09a9feb9b001ca92b0d5b3f0b32633b49
Matina Shrestha <shresthamatina21@gmail.com>
2023-06-02 11:41:18 -0500

    CampsitesDirectoryPage and CampsiteDetail

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,4 +1,3 @@
-//import { CAMPSITES } from "../../app/shared/CAMPSITES";
 import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from "./campsitesSlice";
