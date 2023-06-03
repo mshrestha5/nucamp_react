323e969817944e4597658ba89643dffb0c181685
Matina Shrestha <shresthamatina21@gmail.com>
2023-06-03 11:29:14 -0500

    react week 2 task 3

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -2,6 +2,7 @@ import { Col, Row } from "reactstrap";
 import DisplayCard from "./DisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
+import { selectFeaturedPartner } from "../partners/partnersSlice";
 
 const DisplayList = () => {
   const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
@@ -15,6 +16,9 @@ const DisplayList = () => {
           </Col>
         );
       })}
+      <Col md className="m-1">
+        <DisplayCard item={selectFeaturedPartner()} />
+      </Col>
     </Row>
   );
 };
