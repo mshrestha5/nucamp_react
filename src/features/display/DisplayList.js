6dde90780f8b504d976e3cd1f8fe69bbd673ab69
Matina Shrestha <shresthamatina21@gmail.com>
2023-06-09 12:08:35 -0500

    AnimatedDisplayCard

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,24 +1,30 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from "./DisplayCard";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
 
 const DisplayList = () => {
-  const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+  const items = [
+    selectFeaturedCampsite(),
+    selectFeaturedPromotion(),
+    selectFeaturedPartner(),
+  ];
 
   return (
     <Row>
       {items.map((item, idx) => {
         return (
-          <Col md className="m-1" key={idx}>
-            <DisplayCard item={item} />
-          </Col>
+          item && (
+            <Col md className="m-1" key={idx}>
+              <AnimatedDisplayCard item={item} />
+            </Col>
+          )
         );
       })}
-      <Col md className="m-1">
+      {/* <Col md className="m-1">
         <DisplayCard item={selectFeaturedPartner()} />
-      </Col>
+      </Col> */}
     </Row>
   );
 };
