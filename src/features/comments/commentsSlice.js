0bd06f7536d7fc7d120e805549f42da3ea24d5ed
Matina Shrestha <shresthamatina21@gmail.com>
2023-06-25 08:56:33 -0500

    week 5 task 1

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,26 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { COMMENTS } from "../../app/shared/COMMENTS";
+//import { COMMENTS } from "../../app/shared/oldData/COMMENTS";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { baseUrl } from "../../app/shared/baseUrl";
+
+export const fetchComments = createAsyncThunk(
+  "comments/fetchComments",
+  async () => {
+    const response = await fetch(`${baseUrl}/comments`);
+
+    //const response = await fetch(baseUrl + "comments ");
+    //return response.json();
+
+    if (!response.ok) {
+      return Promise.reject("Unable to fetch, status: " + response.status);
+    } else {
+      const data = await response.json();
+      return data;
+    }
+  }
+);
 
 const initialState = {
-  commentsArray: COMMENTS,
+  commentsArray: [],
+  isLoading: true,
+  errMsg: "",
 };
 
 const commentsSlice = createSlice({
   name: "comments",
   initialState,
-  reducers: {
-    addComment: (state, action) => {
-      console.log("addComment action.payload:", action.payload);
-      console.log("addComment state.commentsArray:", state.commentsArray);
-      const newComment = {
-        id: state.commentsArray.length + 1,
-        ...action.payload,
-      };
-      state.commentsArray.push(newComment);
+  reducers: {},
+  extraReducers: {
+    [fetchComments.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchComments.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = "";
+    },
+    [fetchComments.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = action.error ? action.error.message : "Fetch failed";
     },
   },
 });
-
 export const commentsReducer = commentsSlice.reducer;
 
 export const { addComment } = commentsSlice.actions;
